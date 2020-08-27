c3b55792653898d9626c08a250cf791c17ce4f16
ruhaim <ruhaim@gmail.com>
2020-08-27 08:28:04 +0530

    bump up matrial

--- frontend/src/app/Breadcrumbs.js
+++ frontend/src/app/Breadcrumbs.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Breadcrumbs from "@material-ui/lab/Breadcrumbs";
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from "@material-ui/core/Link";
 import Route from "react-router/Route";
 import { Link as RouterLink } from "react-router-dom";
